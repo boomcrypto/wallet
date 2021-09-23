a114404baa9bbc29cd09b88e5a358961c7a7ff54
Amr Elghobary <amr.elghobary@gmail.com>
2021-09-23 19:03:02 +0200

    add react route and prepare the project for more pages.

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,21 @@ import './App.css';
 import Settings from './screens';
 import {ThemeProvider} from '@mui/material';
 import boomTheme from './theme';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import BoomToolbar from './components/BoomToolbar';
 
 function App() {
   return (
     <ThemeProvider theme={boomTheme}>
-      <Settings />
+      <BoomToolbar title={'Boom Settings'} />
+
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Settings />
+          </Route>
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import {Box} from '@mui/material';
 import AccountSettingsCard from '../components/AccountCardSettings';
 import MoreSettingsCard from '../components/MoreSettingsCard';
-import BoomToolbar from '../components/BoomToolbar';
 
 const Settings: React.FC = () => {
   return (
     <div className="App">
-      <BoomToolbar title={'Boom Settings'} />
       <Box
         sx={{
           padding: 1,
